67fbf2fcb0e5fea279fc6dc454595e63105c9b15
SalmaWR <Salma.wagdy@hotmail.com>
2022-12-14 22:22:51 +0200

    updated

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - print _putchar
+* Description : Write a program that prints _putchar, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char printer[8] = "_putcahr";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(printer[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
