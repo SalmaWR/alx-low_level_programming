a4e09b802b0c81a97691d62fa0164f1439e7025f
SalmaWR <Salma.wagdy@hotmail.com>
2022-12-15 00:26:34 +0200

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,17 @@
 *
 * Return: Always 0 (Success).
 */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	int n;
-	
-	for (i = 0; i <=10; i++)
+
+	for (i = 0; i <= 10; i++)
 	{
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
 		_putchar('\n');
-	}	
-
+	}
 }
