f00d0a99f77d27d3cbf06317603c48b88bd95f8d
SalmaWR <Salma.wagdy@hotmail.com>
2022-12-15 00:22:59 +0200

    nested

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* Description: print_alphabet_x10 - 10 times the alphabet in lowercase.
+*
+* Return: Always 0 (Success).
+*/
+void print_alphabet_x10(void);
+{
+	int i;
+	int n;
+	
+	for (i = 0; i <=10; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}	
+
+}
