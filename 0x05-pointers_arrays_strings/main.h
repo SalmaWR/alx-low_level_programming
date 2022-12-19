029457ad21a5ff30d437b2a7dc56e85ef95eca58
SalmaWR <Salma.wagdy@hotmail.com>
2022-12-20 01:53:03 +0200

    update

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+fndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
+void reset_to_98(int *n);
+void _puts(char *str);
+#endif /*MAIN_H*/
